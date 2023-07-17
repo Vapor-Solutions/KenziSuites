a38a2209f0be81cdb1875d755a2afeef29f8270b
Errol Onzere Kitsiiri <errolantonio325@gmail.com>
2023-07-17 23:22:27 +0300

    Update SendContactEmail.php

--- app/Mail/SendContactEmail.php
+++ app/Mail/SendContactEmail.php
@@ -10,7 +10,7 @@ use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class SendContactEmail extends Mailable
+class SendContactEmail extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
     public $emailData;
